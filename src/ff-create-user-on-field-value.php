5d5c831ccd10aa0081b90fa9e1aeffb68ced3675
Rija Ménagé <git@rijam.sent.as>
2016-08-31 11:45:30 +0800

    added Worpdress plugin header to the entry point php file

---
+++ src/ff-create-user-on-field-value.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package FF_Create_User_On_Field_Value_Plugin
+ * @version 1.0
+ */
+/*
+Plugin Name: Create User On Field Value For Formidable Forms
+Plugin URI: https://cinecinetique.com
+Description: Add functions to a Formidable form for created a user based on the value of field in a form.
+Author: Rija Ménagé
+Version: 1.0
+Author URI: https://cinecinetique.com
+*/
+
+
+
+?>
