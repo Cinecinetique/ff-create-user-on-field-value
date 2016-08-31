c4d852b953a96548ca27bde501540992a3419a0d
Rija Ménagé <git@rijam.sent.as>
2016-08-31 17:24:34 +0800

    Created the entrypoint to the plugin in which our code will be registred as a Wordpress hook

---
+++ src/classes/cinecinetique/wordpress/ffpro/ChangeUserRole.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Cinecinetique\Wordpress\FFPro ;
+
+Class ChangeUserRole {
+  
+}
---
+++ src/ff-change-user-role.php
@@ -0,0 +1,35 @@
+<?php
+namespace Cinecinetique\Wordpress\FFPro;
+/**
+ * @package Change_User_Role
+ * @version 1.0
+ */
+/*
+Plugin Name: Change User Role On Field Value For Formidable Forms
+Plugin URI: https://cinecinetique.com
+Description: Add functions to a Formidable form to change the role of a user when a field on a form has a specific value
+Author: Rija Ménagé
+Version: 1.0
+Author URI: https://www.cinecinetique.com
+*/
+
+global $wpdb, $frmdb;
+
+require_once (__DIR__ . '/classes/cinecinetique/wordpress/ffpro/ChangeUserRole.php') ;
+
+
+Class ChangeUserRoleMain {
+
+  function register_wordpress_hook ($wpdb, $frmdb) {
+
+    $ff_plugin_class = new \Cinecinetique\Wordpress\FFPro\ChangeUserRole($wpdb, $frmdb);
+
+    add_action('frm_after_create_entry', array ($ff_plugin_class, 'changeUserRole'), 45, 2);
+
+  }
+}
+
+
+$main = new ChangeUserRoleMain () ;
+
+$main->register_wordpress_hook ($wpdb, $frmdb) ;
