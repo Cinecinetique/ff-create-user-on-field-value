58f5b2a6259a143ad66234213048c26c49d9de3e
Rija Ménagé <git@rijam.sent.as>
2016-08-31 17:51:23 +0800

    started the main class for the plugin with property and method to access Wordpress and Formidable Pro features

--- src/classes/cinecinetique/wordpress/ffpro/ChangeUserRole.php
+++ src/classes/cinecinetique/wordpress/ffpro/ChangeUserRole.php
@@ -3,5 +3,21 @@
 namespace Cinecinetique\Wordpress\FFPro ;
 
 Class ChangeUserRole {
-  
+
+    private $_wpdb;
+    private $_frmdb;
+
+    public function __construct($wpdb,$frmdb) {
+        $this->_wpdb = $wpdb;
+        $this->_frmdb = $frmdb;
+    }
+    
+    public function get_wpdb () {
+      return $this->_wpdb;
+    }
+
+    public function get_frmdb () {
+        return $this->_frmdb;
+    }
+
 }
--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -6,6 +6,7 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Cinecinetique\\Wordpress\\FFPro\\ChangeUserRole' => $baseDir . '/src/classes/cinecinetique/wordpress/ffpro/ChangeUserRole.php',
     'Cinecinetique\\Wordpress\\FFPro\\ChangeUserRoleMain' => $baseDir . '/src/ff-change-user-role.php',
     'File_Iterator' => $vendorDir . '/phpunit/php-file-iterator/src/Iterator.php',
     'File_Iterator_Facade' => $vendorDir . '/phpunit/php-file-iterator/src/Facade.php',
